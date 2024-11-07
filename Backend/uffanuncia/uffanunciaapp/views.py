09ad87387441ee77729c7f93395dbfbf68218d5c
TrackThTrail <45440895+TrackThTrail@users.noreply.github.com>
2024-11-07 14:58:33 -0300

    FEAT-2-anuncio

--- Backend/uffanuncia/uffanunciaapp/views.py
+++ Backend/uffanuncia/uffanunciaapp/views.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
-from .models import Anuncio
+from .models import Anuncio, Usuario
 from .serializers import AnuncioSerializer
+import json
 
 class AnuncioViewSet(viewsets.ModelViewSet):
     queryset = Anuncio.objects.all()
     serializer_class = AnuncioSerializer
 
-    def create(request):
-        return HttpResponse("Olá, mundo!")
+    def create(self, request):
+        nome = request.data.get('nome')
+        categoria = request.data.get('categoria')
+        valor = request.data.get('valor')
+        usuario = Usuario.objects.first()
+        anuncio = Anuncio(nome=nome, categoria=categoria, valor=valor, usuario=usuario)
+        anuncio.save()
+        return HttpResponse(status=200)
     
     def delete(request):
         return HttpResponse("Olá, mundo!")
