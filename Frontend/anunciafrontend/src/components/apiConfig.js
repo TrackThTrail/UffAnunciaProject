c77de30f06c088676d49e09ecd4a83c8c109219b
TrackThTrail <45440895+TrackThTrail@users.noreply.github.com>
2024-11-11 15:19:48 -0300

    muda para heroku prod

--- Frontend/anunciafrontend/src/components/apiConfig.js
+++ Frontend/anunciafrontend/src/components/apiConfig.js
@@ -1,3 +1,3 @@
-const apiUrl = 'http://localhost:8000';
-//const apiUrl = 'https://anunciauffheroku-b998b85f5dfd.herokuapp.com';
+//const apiUrl = 'http://localhost:8000';
+const apiUrl = 'https://anunciauffheroku-b998b85f5dfd.herokuapp.com';
 export default apiUrl;
